4e12f8c867643cc84a2693c92ed752721365f0da
lucasPimentel <lucaspimenteldb@gmail.com>
2020-11-20 13:26:44 -0300

    section 3

--- section3/app.js
+++ section3/app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
-// eventdriven - createServer precisa de argumentos para requisição feita e resposta a ser dado pelo servidor
+// eventdriven e eventlisteners
+// createServer precisa de argumentos para requisição feita e resposta a ser dado pelo servidor
 const server = http.createServer((req, res) => {
   // apresentando o objeto request
   // a cada novo request o servidor loga estas informações
@@ -11,8 +12,8 @@ const server = http.createServer((req, res) => {
   // trabalhando com a resposta que o servidor dará
   res.setHeader('Content-type', 'text/html')
 
-  res.write('<html><head><title>Piroca</title></head>')
-  res.write('<body><h1>Você acessou a melhor piroca</h1></body></html>')
+  res.write('<html><head><title>Piroca</title></head>');
+  res.write('<body><h1>Você acessou a melhor piroca</h1></body></html>');
   res.end();
 });
 
